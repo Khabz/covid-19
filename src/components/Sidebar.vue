7b26b398ea9bef25c23e928a80544a200322a842
Khabz <khabubundivhu@gmail.com>
2020-03-16 20:37:55 +0200

    :sparkles: visualise stats by country

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="topnav">
+    <div class="container-fluid">
+      <nav class="navbar navbar-light navbar-expand-lg topnav-menu">
+        <div class="collapse navbar-collapse" id="topnav-menu-content">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">
+                <i class="bx bx-home-circle mr-2"></i>Dashboard
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="home">
+    <div class="row">
+      <div class="col-12">
+        <div
+          class="page-title-box d-flex align-items-center justify-content-between"
+        >
+          <h4 class="mb-0 font-size-18">Dashboard</h4>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <ol class="breadcrumb m-0">
+        <label>Select Country</label>
+        <select
+          class="form-control"
+          v-model="selected_country"
+          @click="getStatsByCountry(selected_country)"
+        >
+          <option
+            v-for="(country, index) in countries"
+            :key="index"
+            :value="country"
+          >
+            {{ index }}
+          </option>
+        </select>
+      </ol>
+      </div>
+      <div v-if="selected_country !== null" track-by="id" class="col-xl-12 mt-3">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card mini-stats-wid bg-warning">
+              <div class="card-body">
+                <div class="media">
+                  <div class="media-body">
+                    <h5 class="text-white">Confirmed Cases</h5>
+                    <h4 class="mb-0">{{ results.confirmed.value }}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mini-stats-wid bg-success">
+              <div class="card-body">
+                <div class="media">
+                  <div class="media-body">
+                    <h5 class="text-white">Recovered</h5>
+                    <h4 class="mb-0">{{ results.recovered.value }}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mini-stats-wid bg-danger">
+              <div class="card-body">
+                <div class="media">
+                  <div class="media-body">
+                    <h5 class="text-white">Deaths</h5>
+                    <h4 class="mb-0">{{ results.deaths.value }}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end row -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      countries: [],
+      selected_country: null,
+      results: []
+    };
+  },
+  methods: {
+    getStatsByCountry(val) {
+      if(val == null) {
+        return null
+      } else {
+        axios
+          .get("https://covid19.mathdro.id/api/countries/" + val)
+          .then(response => {
+            this.results = response.data;
+          });
+      }
+    }
+  },
+  mounted() {
+    axios.get("https://covid19.mathdro.id/api/countries").then(response => {
+      this.countries = response.data.countries;
+    });
+  }
+};
+</script>
